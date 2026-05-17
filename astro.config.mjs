cbdd11cd608ba94d8143764752701d6499e536f3
Daan <daansol@hotmail.com>
2026-05-17 12:41:57 -0600

    Complementary info

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,8 +9,6 @@ import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: netlify(),
   integrations: [tailwind(), react()],
   devToolbar: {
     enabled: false
